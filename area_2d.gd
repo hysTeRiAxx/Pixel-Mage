cf8d8439549b7e1909ec76e07506d8ddb13d5534
hysTeRiAxx <kzdunich115@edu.cepeo.on.ca>
2025-09-28 07:53:45 -0400

    Finishing the game

--- area_2d.gd
+++ area_2d.gd
@@ -1,9 +1,7 @@
 extends Area2D
 
-# Called when another body enters the coin's area
-func _on_Coin_body_entered(body):
-	# Check if the body is the player (change "Player" to your player node's name)
-	if body.name == "Player":
+func _ready():
+	body_entered.connect(_on_body_entered)
 
-		# Remove the coin from the game scene
-		queue_free()
+func _on_body_entered(body):
+	queue_free()  
--- player.gd
+++ player.gd
@@ -1,8 +1,23 @@
 extends CharacterBody2D
 
-@export var speed = 400
+const SPEED = 300.0
+const JUMP_VELOCITY = -750.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * speed
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Jump
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get left/right input
+	var direction = Input.get_axis("left", "right")
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
 	move_and_slide()
